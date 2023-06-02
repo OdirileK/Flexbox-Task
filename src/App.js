3ba21837a67bb56e156d2bbc1d56418b8b34fd7e
OdirileK <emkayphozur@gmail.com>
2023-06-02 10:29:26 +0200

    a

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./index.css";
+
+function App() {
+  return (
+ 
+    <div className="container">
+      <div className="header-container">
+          <div className="flexbox-item1"></div>
+          <div className="flexbox-item2"></div>
+          <div className="flexbox-item3"></div>
+          </div>
+    
+      <div className="middle-container">
+        <div className="flex-box1"></div>
+        <div className="flex-box2"></div>
+        <div className="flex-box3"></div>
+        </div>
+  
+        <div className="footer-container">
+        </div>
+
+    </div>
+
+    
+  );
+}
+
+export default App;
